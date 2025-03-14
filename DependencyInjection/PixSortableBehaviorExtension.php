8e209c4235e90e4d3c542671ce118fe7bdc775d1
Damien Matabon <damien.matabon@gmail.com>
2025-03-14 19:06:47 +0100

    fix deprecation

--- DependencyInjection/PixSortableBehaviorExtension.php
+++ DependencyInjection/PixSortableBehaviorExtension.php
@@ -24,10 +24,7 @@ use Symfony\Component\DependencyInjection\Alias;
  */
 class PixSortableBehaviorExtension extends Extension
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
